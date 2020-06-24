29a6abaaa086a31a6fcf9160a696d72ead8d6ab1
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-06-24 19:12:44 +0300

    cleaning the code

--- abdulbasit-hussein-yazen/src/App.js
+++ abdulbasit-hussein-yazen/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
--- abdulbasit-hussein-yazen/src/Components/Main.js
+++ abdulbasit-hussein-yazen/src/Components/Main.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Main() {
-  return (
-    <main>
-      <h1>hello from Main</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1>hello from Main</h1>
+        </main>
+    );
 }
 
 export default Main;
